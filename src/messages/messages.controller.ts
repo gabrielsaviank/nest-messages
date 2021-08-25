89e831cdad4ac1f4530801b651dfd3a3d1d23f1b
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-08-25 08:41:47 -0300

    feat:dependency injection implemented

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -4,13 +4,8 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
 
-  constructor() {
-    // DONT DO THIS ON REAL APP
-    // USE DEPENDENCY INJECTION
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
 
   @Get()
   listMessages() {
